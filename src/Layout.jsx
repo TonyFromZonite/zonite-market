bbcc2edd72c283ab38db8993e7f449dfc0ada0d4
tony kodjeu <tonykodjeu@gmail.com>
2026-03-04 01:55:05 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,7 +4,7 @@ import { createPageUrl } from "@/utils";
 import {
   LayoutDashboard, ShoppingCart, Package, Users, Truck,
   ClipboardList, DollarSign, Shield, Menu, X, ChevronRight,
-  LogOut, Tag, Bell, TrendingUp, RotateCcw, MessageSquare
+  LogOut, Tag, Bell, MessageSquare
 } from "lucide-react";
 import { base44 } from "@/api/base44Client";
 import RechercheGlobale from "@/components/RechercheGlobale";
