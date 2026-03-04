323331ac240ba4ed9765678ee97355cdd01389b1
tony kodjeu <tonykodjeu@gmail.com>
2026-03-04 21:49:13 +0000

    File changes

--- src/pages/TableauDeBord.jsx
+++ src/pages/TableauDeBord.jsx
@@ -168,26 +168,26 @@ function DashboardAdmin() {
 
   const enChargement = chargementVentes || chargementProduits || chargementVendeurs;
 
-  const chiffreAffaires = ventes
+  const chiffreAffaires = (ventes || [])
     .filter(v => v.statut_commande !== "annulee" && v.statut_commande !== "retournee")
     .reduce((s, v) => s + (v.montant_total || 0), 0);
 
-  const profitNet = ventes
+  const profitNet = (ventes || [])
     .filter(v => v.statut_commande !== "annulee" && v.statut_commande !== "retournee")
     .reduce((s, v) => s + (v.profit_zonite || 0), 0);
 
-  const commissionsAPayer = vendeurs.reduce((s, v) => s + (v.solde_commission || 0), 0);
-  const stockCritique = produits.filter(p => (p.stock_global || 0) <= (p.seuil_alerte_global || 5)).length;
+  const commissionsAPayer = (vendeurs || []).reduce((s, v) => s + (v.solde_commission || 0), 0);
+  const stockCritique = (produits || []).filter(p => (p.stock_global || 0) <= (p.seuil_alerte_global || 5)).length;
 
   const aujourdhui = new Date().toISOString().split("T")[0];
-  const commandesDuJour = ventes.filter(v => {
+  const commandesDuJour = (ventes || []).filter(v => {
     const d = v.date_vente ? v.date_vente.split("T")[0] : v.created_date?.split("T")[0];
     return d === aujourdhui;
   }).length;
 
-  const topProduit = [...produits].sort((a, b) => (b.total_vendu || 0) - (a.total_vendu || 0))[0];
-  const commandesVendeursAujourdhui = commandesVendeurs.filter(c => c.created_date?.split("T")[0] === aujourdhui).length;
-  const commissionsVendeursAPayer = paiementsEnAttente.reduce((s, p) => s + (p.montant || 0), 0);
+  const topProduit = [...(produits || [])].sort((a, b) => (b.total_vendu || 0) - (a.total_vendu || 0))[0];
+  const commandesVendeursAujourdhui = (commandesVendeurs || []).filter(c => c.created_date?.split("T")[0] === aujourdhui).length;
+  const commissionsVendeursAPayer = (paiementsEnAttente || []).reduce((s, p) => s + (p.montant || 0), 0);
 
   const formaterMontant = (n) => `${Math.round(n).toLocaleString("fr-FR")} FCFA`;
 
