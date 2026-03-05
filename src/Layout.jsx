9b76163679e456ccbb1095274ea06988d51f08ef
tony kodjeu <tonykodjeu@gmail.com>
2026-03-05 17:06:53 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -9,6 +9,7 @@ import {
 import { base44 } from "@/api/base44Client";
 import RechercheGlobale from "@/components/RechercheGlobale";
 import NotificationBell from "@/components/NotificationBell";
+import { NotificationToaster } from "@/components/NotificationSystem";
 
 // Pages de l'Application Vendeur (interface vendeur mobile, sans sidebar admin)
 const PAGES_VENDEUR = [
