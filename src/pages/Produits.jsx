a316cd5c1adc0ddfb852fe4c9192f4c6cdf70a2e
tony kodjeu <tonykodjeu@gmail.com>
2026-03-05 13:11:57 +0000

    File changes

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
-import { useCachedQuery } from "@/components/CacheManager";
+import { useCachedQuery, invalidateCache } from "@/components/CacheManager";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
