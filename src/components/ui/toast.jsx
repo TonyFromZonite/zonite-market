a48cadbd6574665e6cdeaaaf2d9fd4621eef83b0
tony kodjeu <tonykodjeu@gmail.com>
2026-03-13 19:58:44 +0000

    File changes

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -1,21 +1,18 @@
 import * as React from "react";
+import * as ToastPrimitive from "@radix-ui/react-toast";
 import { cva } from "class-variance-authority";
 import { X } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-const ToastProvider = React.forwardRef(({ ...props }, ref) => (
-  <div
-    ref={ref}
-    className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]"
-    {...props}
-  />
-));
-ToastProvider.displayName = "ToastProvider";
+const ToastProvider = ToastPrimitive.Provider;
 
-const ToastViewport = React.forwardRef(({ ...props }, ref) => (
-  <div
+const ToastViewport = React.forwardRef(({ className, ...props }, ref) => (
+  <ToastPrimitive.Viewport
     ref={ref}
-    className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]"
+    className={cn(
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      className
+    )}
     {...props}
   />
 ));
@@ -101,4 +98,4 @@ export {
   ToastDescription,
   ToastClose,
   ToastAction,
-}; 
+};
